4341be0f607cb4b6c6f8a5abc3484cda56f2492e
Roger Matamoros <88455490+Slatter28@users.noreply.github.com>
2022-09-21 21:44:12 -0500

    sentencias

--- sentences.sql
+++ sentences.sql
@@ -8,7 +8,7 @@ CREATE TABLE "tasks" (
 
 
 
-INSERT INTO public.tasks
+INSERT INTO tasks
 (id, name_task, description, state, start_date)
 VALUES('6b984a8c-8dc7-42fa-933d-53442d75312c', 'Hacer deberes', 'Hacer todas los deberes de academlo', 'iniciar', '2022/10/21'),
 ('afea1d03-b4a4-4ee4-a31e-398eb66daa19', 'Limpiar la casa', 'Barrer la casa y limpiar los baños', 'iniciar', '2022/10/20'),
