fe91dfc461b7916ed90081fd9117121971ef92ae
Roger Matamoros <88455490+Slatter28@users.noreply.github.com>
2022-09-26 21:25:03 -0500

    agregadas sentencias de join

--- sentences-join-bank.sql
+++ sentences-join-bank.sql
@@ -1,8 +1,8 @@
 select c.id as id_cliente, c.first_name as nombre, c.last_name as apellido, c.email as email, a.account_no as cuenta, a.balance as saldo from clients c left join accounts a  on c.id = a.client_id ;
 
-select a.account_no as cuenta, a.client_id as "id cliente" , at2."name" as "tipo cuenta" from accounts a left join account_types at2 on a."type"  = at2.id 
+select a.account_no as cuenta, a.client_id as "id cliente" , at2."name" as "tipo cuenta" from accounts a left join account_types at2 on a."type"  = at2.id ;
 
-select t.id as transaccion , t.amount as monto , tt."name" as "tipo de transaccion" from transactions t left join transactions_types tt on t.transaction_type = tt.id  
+select t.id as transaccion , t.amount as monto , tt."name" as "tipo de transaccion" from transactions t left join transactions_types tt on t.transaction_type = tt.id  ;
 
 select t.id as transaccion , t.amount as monto , tt."name" as "tipo de transaccion" from transactions_types tt 
 left join transactions t  on t.transaction_type = tt.id;
